da296615463e92dd659abe6b07def592172604e7
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2016-04-21 17:57:21 +0200

    added support for multiple loggers with different configs

--- test/index.js
+++ test/index.js
@@ -1,53 +1,67 @@
 'use strict';
 
-const Logger = require('../lib');
-const os = require('os');
+const assert = require('chai').assert;
+const loggerLib = require('../lib');
 const path = require('path');
 const fs = require('fs');
+const util = require('util');
+const logFile = __dirname + path.sep + 'test.log';
 
 describe('tests', function() {
-  it('instanciate with default config', function(done) {
+  before(function(done) {
     try {
-      const logger = Logger();
-      done();
-    } catch (e) {
-      done(e);
-    }
-  });
-  it('instanciate with invalid config', function(done) {
-    try {
-      const logger = Logger('abc');
-      done('should throw an error');
-    } catch (e) {
-      console.log(e);
-      done();
-    }
-  });
-  it('instanciate with custom config', function(done) {
-    try {
-      const filename = os.tmpDir() + path.sep + 'default.log';
-      const logger = Logger({
-        transports: [
-          {
-            type: 'File',
-            options: {
-              name: 'default',
-              filename: filename,
-              level: 'info',
-            },
-          },
-        ],
+      fs.unlink(logFile, function() {
+        done();
       });
-      logger.info('test');
+    } catch (e) {
+      done();
+    }
+  });
+  it('log as different authors', function(done) {
+    try {
+      const loggers = {
+        one: loggerLib({
+          author: 'one',
+          level: 'debug',
+          filename: logFile,
+        }),
+        two: loggerLib({
+          author: 'two',
+          level: 'debug',
+          filename: logFile,
+        }),
+      };
+      const msg = {
+        one: [
+          'message from author one',
+          'another message from author one with meta',
+          {author: 'one', meta: 'data'},
+        ],
+        two: [
+          'author two message',
+          'another two again but with meta data',
+          {author: 'two', meta: 'data'},
+        ],
+      };
+      loggers.one.info(msg.one[0]);
+      loggers.one.info(msg.one[1], msg.one[2]);
+
+      loggers.two.info(msg.two[0]);
+      loggers.two.info(msg.two[1], msg.two[2]);
+
       setTimeout(function() {
-        fs.stat(filename, (err, stats) => {
+        fs.readFile(logFile, 'utf8', (err, data) => {
           if (err) {
             done(err);
-          } else if ( !stats.isFile(filename) ) {
-            done('file not found');
-          } else {
-            done();
           }
+          assert(data.indexOf(msg.one[0]) !== -1);
+          assert(data.indexOf(msg.one[1]) !== -1);
+          assert(data.indexOf(util.inspect(msg.one[2], false, 5, false)) !== -1);
+
+          assert(data.indexOf(msg.two[0]) !== -1);
+          assert(data.indexOf(msg.two[1]) !== -1);
+          assert(data.indexOf(util.inspect(msg.two[2], false, 5, false)) !== -1);
+          done();
         });
       }, 500);
     } catch (e) {
