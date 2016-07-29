3c37bf5fa1408bc4b149b2d354274d7a134f638c
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2016-07-29 18:15:38 +0200

    updated doc & samples

--- samples/default.js
+++ samples/default.js
@@ -1,6 +1,12 @@
 'use strict';
-const loggerLib = require('cta-logger');
-const logger = loggerLib();
-logger.info('Hi there...');
-logger.log('info', 'Hi again!');
-logger.error('An error occured!');
+
+const Logger = require('cta-logger');
+const logger = new Logger();
+const foo = logger.author('foo');
+const bar = logger.author('bar');
+logger.info('Hi there');
+// 2016-07-29T16:07:05.259Z - info - DESKTOP-8ONSCVQ - UNKNOWN - "Hi there"
+foo.info('Hi, i am foo');
+// 2016-07-29T16:07:05.263Z - info - DESKTOP-8ONSCVQ - FOO - "Hi, i am foo"
+bar.info('Hi, and i am bar');
+// 2016-07-29T16:07:05.264Z - info - DESKTOP-8ONSCVQ - BAR - "Hi, and i am bar"
